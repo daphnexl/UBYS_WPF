eb151c99e951dd2bcbfb35fbab878664de56cf5e
irmakoflu <irmakoflu@icloud.com>
2025-02-18 20:57:06 +0300

    ilk adım
    tasarım deseni oluşturuldu

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using UBYS_WPF.MVVM.ViewModels;
+using UBYS_WPF.MVVM.Views;
 
 namespace UBYS_WPF
 {
@@ -9,6 +11,22 @@ namespace UBYS_WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // ViewModel oluştur
+            var mainVM = new MainVM();
+
+            // View oluştur ve DataContext olarak ViewModel'i bağla
+            var mainView = new MainView
+            {
+                DataContext = mainVM
+            };
+
+            // Pencereyi göster
+            mainView.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
--- MVVM/ViewModels/MainVM.cs
+++ MVVM/ViewModels/MainVM.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UBYS_WPF.Cores;
 
 namespace UBYS_WPF.MVVM.ViewModels
 {
-    public class MainVM
+    public class MainVM : ViewModelBase
     {
+		private string _data;
+		public string Data
+		{
+			get { return _data; }
+			set
+			{
+				_data = value;
+				OnPropertyChanged(nameof(Data));
+			}
+		}
+
+		public MainVM() {
+			Data = "Kullanıcı Adı";
+		}
     }
 }
