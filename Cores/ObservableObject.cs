8b308891ded48f1b3d2cce7d6c998f1e04fc2d62
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-03-01 22:28:21 +0300

    klasörler
    klasör düzenlemeleri

---
+++ Cores/ObservableObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UBYS_WPF.Cores
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            // Correct usage of 'this' and 'PropertyChanged'
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
