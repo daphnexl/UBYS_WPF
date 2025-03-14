31a506a96f3eb3e98ff78aa9a7e2a1c118e9b40a
Defne Çivrilli <119342607+daphnexl@users.noreply.github.com>
2025-03-14 14:07:48 +0300

    created MyScore user control

--- MVVM/ViewModels/MyScoreVM.cs
+++ MVVM/ViewModels/MyScoreVM.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace UBYS_WPF.MVVM.ViewModels
+public class MyScoreViewModel : INotifyPropertyChanged
 {
-    public class MyScore
+    public ObservableCollection<GradeEntry> Grades { get; set; }
+
+    public MyScoreViewModel()
     {
+        Grades = new ObservableCollection<GradeEntry>
+            {
+                new GradeEntry { CourseName = "Mathematics", Grade = "A", TeacherName = "Dr. AAA", DateReceived = DateTime.Now.ToShortDateString() },
+                new GradeEntry { CourseName = "Algorithm", Grade = "B", TeacherName = "Prof. BBB", DateReceived = DateTime.Now.ToShortDateString() }
+            };
     }
+
+    public void AddGrade(string course, string grade, string teacher)
+    {
+        Grades.Add(new GradeEntry { CourseName = course, Grade = grade, TeacherName = teacher, DateReceived = DateTime.Now.ToShortDateString() });
+        OnPropertyChanged(nameof(Grades));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+public class GradeEntry
+{
+    public string CourseName { get; set; }
+    public string Grade { get; set; }
+    public string TeacherName { get; set; }
+    public string DateReceived { get; set; }
 }
